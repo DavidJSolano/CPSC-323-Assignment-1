9d75aa08453c401967536b8b7df662e3f8715fb7
David Solano <111948487+DavidJSolano@users.noreply.github.com>
2024-05-11 20:20:47 -0700

    Update Formatting and Automatic Test Case Updates
    Created a system where all of the test cases are updated automatically.

--- assignment3/main.py
+++ assignment3/main.py
@@ -13,6 +13,75 @@ def restore_stdout() -> None:
 
 
 while True:
+    # Test Case 1
+    store_stdout()
+    testcase = Syntax(FSM("test1.txt"))
+    testcase.Rat24S(testcase.token_list[0])
+    sa_output: str = output_stream.getvalue()
+    i = 1
+    item_list = ""
+    for item in testcase.assembly:
+        print(f"{i}. {item}")
+        item_list += item + '\n'
+        i += 1
+    item_list += "------------- Symbol Table -------------\n"
+    item_list += f"Identifier:\t\t\t\tMemoryLocation:\t\t\tType:\n"
+    print("------------- Symbol Table -------------")
+    print(f"Identifier:\t\tMemoryLocation:\t\tType:")
+    for item, value in testcase.symbol_table.items():
+        item_list += f"{item:<24}{value:<24}{testcase.types[item]}\n"
+        print(f"{item:<24}{value:<24}{testcase.types[item]}")
+    f = open("output1.txt", 'w')
+    f.write(item_list)
+    f.close()
+    restore_stdout()
+
+    # Test Case 2
+    store_stdout()
+    testcase = Syntax(FSM("test2.txt"))
+    testcase.Rat24S(testcase.token_list[0])
+    sa_output: str = output_stream.getvalue()
+    i = 1
+    item_list = ""
+    for item in testcase.assembly:
+        print(f"{i}. {item}")
+        item_list += item + '\n'
+        i += 1
+    item_list += "------------- Symbol Table -------------\n"
+    item_list += f"Identifier:\t\t\t\tMemoryLocation:\t\t\tType:\n"
+    print("------------- Symbol Table -------------")
+    print(f"Identifier:\t\tMemoryLocation:\t\tType:")
+    for item, value in testcase.symbol_table.items():
+        item_list += f"{item:<24}{value:<24}{testcase.types[item]}\n"
+        print(f"{item:<24}{value:<24}{testcase.types[item]}")
+    f = open("output2.txt", 'w')
+    f.write(item_list)
+    f.close()
+    restore_stdout()
+
+    # Test Case 3
+    store_stdout()
+    testcase = Syntax(FSM("test3.txt"))
+    testcase.Rat24S(testcase.token_list[0])
+    sa_output: str = output_stream.getvalue()
+    i = 1
+    item_list = ""
+    for item in testcase.assembly:
+        print(f"{i}. {item}")
+        item_list += item + '\n'
+        i += 1
+    item_list += "------------- Symbol Table -------------\n"
+    item_list += f"Identifier:\t\t\t\tMemoryLocation:\t\t\tType:\n"
+    print("------------- Symbol Table -------------")
+    print(f"Identifier:\t\tMemoryLocation:\t\tType:")
+    for item, value in testcase.symbol_table.items():
+        item_list += f"{item:<24}{value:<24}{testcase.types[item]}\n"
+        print(f"{item:<24}{value:<24}{testcase.types[item]}")
+    f = open("output3.txt", 'w')
+    f.write(item_list)
+    f.close()
+    restore_stdout()
+
     print("Please enter an integer value for the test case you would like to run (1, 2, or 3) or 'Q' to exit")
     print("1. test1.txt")
     print("2. test2.txt")
@@ -33,7 +102,7 @@ while True:
                 item_list += item + '\n'
                 i += 1
             item_list += "------------- Symbol Table -------------\n"
-            item_list += f"Identifier:\t\tMemoryLocation:\t\tType:\n"
+            item_list += f"Identifier:\t\t\t\tMemoryLocation:\t\t\tType:\n"
             print("------------- Symbol Table -------------")
             print(f"Identifier:\t\tMemoryLocation:\t\tType:")
             for item, value in testcase.symbol_table.items():
@@ -55,7 +124,7 @@ while True:
                 item_list += item + '\n'
                 i += 1
             item_list += "------------- Symbol Table -------------\n"
-            item_list += f"Identifier:\t\tMemoryLocation:\t\tType:\n"
+            item_list += f"Identifier:\t\t\t\tMemoryLocation:\t\t\tType:\n"
             print("------------- Symbol Table -------------")
             print(f"Identifier:\t\tMemoryLocation:\t\tType:")
             for item, value in testcase.symbol_table.items():
@@ -77,7 +146,7 @@ while True:
                 item_list += item + '\n'
                 i += 1
             item_list += "------------- Symbol Table -------------\n"
-            item_list += f"Identifier:\t\tMemoryLocation:\t\tType:\n"
+            item_list += f"Identifier:\t\t\t\tMemoryLocation:\t\t\tType:\n"
             print("------------- Symbol Table -------------")
             print(f"Identifier:\t\tMemoryLocation:\t\tType:")
             for item, value in testcase.symbol_table.items():
